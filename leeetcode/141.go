efe9bf4db1a2ccfb96702eeda40c4e61a2a8c5d0
lizhenwei.will <lizhenwei.will@foxmail.com>
2022-03-11 17:05:14 +0800

    leetcode 141

---
+++ leeetcode/141.go
@@ -0,0 +1,35 @@
+package leetcode
+
+// 判断链表是否有环
+func hasCycle(head *ListNode) bool {
+	return true
+}
+
+// 快慢指针
+func hasCycleV2(head *ListNode) bool {
+	if head == nil || head.Next == nil {
+		return false
+	}
+	for slow, fast := head, head.Next; fast != slow; {
+		if fast == nil || fast.Next == nil {
+			return false
+		}
+		slow = slow.Next
+		fast = fast.Next.Next
+	}
+	return true
+}
+
+// 暴力解答
+// 用 hash 表存链表的内存地址值
+func hasCycleV1(head *ListNode) bool {
+	m := map[*ListNode]byte{}
+	if head != nil {
+		if _, ok := m[head]; ok {
+			return true
+		}
+		m[head] = byte(1)
+		head = head.Next
+	}
+	return false
+}
---
+++ leeetcode/141_test.go
@@ -0,0 +1,27 @@
+package leetcode
+
+import "testing"
+
+func TestHasCycleV1(t *testing.T) {
+	n7 := ListNode{7, nil}
+	n6 := ListNode{2, &n7}
+	n5 := ListNode{5, &n6}
+	n4 := ListNode{4, &n5}
+	n3 := ListNode{3, &n4}
+	n2 := ListNode{2, &n3}
+	n1 := ListNode{1, &n2}
+	printNodeLise(&n1)
+	t.Log(hasCycleV1(&n1))
+}
+
+func TestHasCycleV2(t *testing.T) {
+	n7 := ListNode{7, nil}
+	n6 := ListNode{2, &n7}
+	n5 := ListNode{5, &n6}
+	n4 := ListNode{4, &n5}
+	n3 := ListNode{3, &n4}
+	n2 := ListNode{2, &n3}
+	n1 := ListNode{1, &n2}
+	printNodeLise(&n1)
+	t.Log(hasCycleV2(&n1))
+}
